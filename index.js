ec38de5fcb43f76d09a7c77f5013c972ce81dfe1
skikhtear <skikhtear@gmail.com>
2022-11-10 10:58:07 +0600

    crud

--- index.js
+++ index.js
@@ -56,6 +56,38 @@ async function run() {
             res.send(services);
         });
 
+        app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        });
+
+        app.get('/allreviews',async(req,res)=>{
+            const query ={};
+            const cursor = reviewCollection.find(query)
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        app.get('/reviews', async (req, res) => {
+            const decoded = req.decoded;
+            console.log('inside review api', decoded);
+
+
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
+        });
+
        
 
         
