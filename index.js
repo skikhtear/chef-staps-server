c1b55c2fdcd55ea8948d133ee744fb9cce12daa2
skikhtear <skikhtear@gmail.com>
2022-11-10 10:59:37 +0600

    crud

--- index.js
+++ index.js
@@ -107,7 +107,12 @@ async function run() {
             res.send(result);
         });
 
-        
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        });
 
         
     }
